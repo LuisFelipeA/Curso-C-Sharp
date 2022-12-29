cc5b26664525b7ff135aa847cb599a2398356462
Luis Felipe <felipeadriani2@gmail.com>
2022-12-29 16:36:08 -0300

    Operadores aritméticos

--- Curso/Curso/Program.cs
+++ Curso/Curso/Program.cs
@@ -5,8 +5,31 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
+            //Operadores aritméticos
+
+            int n1 = 3 + 4 * 2;
+            Console.WriteLine(n1);
+
+            int n2 = (3 + 4) * 2;
+            Console.WriteLine(n2);
+
+            int n3 = 17 % 3;
+            Console.WriteLine(n3);
+
+            double n4 = 10.0 / 8;       //tem que colocar o '.0' para entender que é um double
+            Console.WriteLine(n4);
+
+            double a = 1.0, b = -3.0, c = -4;
+
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double resultado = (-b + Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(resultado);
+
             //Conversão implicita e casting
 
+            /*
             float x = 4.5f;
             double y = x; //aceita normalmente
 
@@ -24,7 +47,7 @@ namespace Course {
 
             double resultado = (double)d / e;
             Console.WriteLine(resultado);
-
+            */
 
 
             //Operadores de Atribuição
