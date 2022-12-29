371aa07bfa7aec07dd29d09353fb6033a625fb00
Luis Felipe <felipeadriani2@gmail.com>
2022-12-29 15:02:14 -0300

    Tipos basicos de dados

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 
-namespace PrimeiroProjeto
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
+namespace PrimeiroProjeto {
+
+    internal class Program {
+
+        static void Main(string[] args) {
+
             Console.WriteLine("Hello World!!");
+            Console.WriteLine("Pressione ENTER para sair...");
+            Console.Read();
+
         }
     }
 }
