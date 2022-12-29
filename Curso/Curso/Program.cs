79094cfb636084a4f424ef6cfe1dd971b5aebea0
Luis Felipe <felipeadriani2@gmail.com>
2022-12-29 16:54:59 -0300

    Entrada de Dados

--- Curso/Curso/Program.cs
+++ Curso/Curso/Program.cs
@@ -5,48 +5,76 @@ namespace Course {
     class Program {
         static void Main(string[] args) {
 
+            //Entrada de Dados
+
+            Console.Write("Seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine($"Ola, {nome}");
+            Console.WriteLine("-------------");
+
+            Console.Write("Digite algo 1: ");
+            string x = Console.ReadLine();
+            Console.Write("Digite algo 2: ");
+            string y = Console.ReadLine();
+            Console.Write("Digite algo 3: ");
+            string z = Console.ReadLine();
+            Console.WriteLine($"{x}, {y}, {z}");
+            Console.WriteLine("-------------");
+
+            Console.WriteLine("Testando comando Split");
+            Console.Write("Digite palavras separando por espaço: ");
+            string[] vet = Console.ReadLine().Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+            Console.WriteLine($"{p1}, {p2}, {p3}");
+
+
             //Operadores aritméticos
+            /*
+                        int n1 = 3 + 4 * 2;
+                        Console.WriteLine(n1);
 
-            int n1 = 3 + 4 * 2;
-            Console.WriteLine(n1);
+                        int n2 = (3 + 4) * 2;
+                        Console.WriteLine(n2);
 
-            int n2 = (3 + 4) * 2;
-            Console.WriteLine(n2);
+                        int n3 = 17 % 3;
+                        Console.WriteLine(n3);
 
-            int n3 = 17 % 3;
-            Console.WriteLine(n3);
+                        double n4 = 10.0 / 8;       //tem que colocar o '.0' para entender que é um double
+                        Console.WriteLine(n4);
 
-            double n4 = 10.0 / 8;       //tem que colocar o '.0' para entender que é um double
-            Console.WriteLine(n4);
+                        double a = 1.0, b = -3.0, c = -4;
 
-            double a = 1.0, b = -3.0, c = -4;
+                        double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
 
-            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+                        double resultado = (-b + Math.Sqrt(delta)) / (2.0 * a);
 
-            double resultado = (-b + Math.Sqrt(delta)) / (2.0 * a);
+                        Console.WriteLine(resultado);
+            */
 
-            Console.WriteLine(resultado);
 
             //Conversão implicita e casting
 
             /*
-            float x = 4.5f;
-            double y = x; //aceita normalmente
+                        float x = 4.5f;
+                        double y = x; //aceita normalmente
 
-            Console.WriteLine(y);
-            double a = 5.3;
+                        Console.WriteLine(y);
+                        double a = 5.3;
 
-            float b = (float)a; //força a variavel para aceitar a outra
-            Console.WriteLine(b);
+                        float b = (float)a; //força a variavel para aceitar a outra
+                        Console.WriteLine(b);
 
-            int c = (int)a; //força para aceitar o inteiro mesmo perdendo informação
-            Console.WriteLine(c);
+                        int c = (int)a; //força para aceitar o inteiro mesmo perdendo informação
+                        Console.WriteLine(c);
 
-            int d = 5;
-            int e = 2;
+                        int d = 5;
+                        int e = 2;
 
-            double resultado = (double)d / e;
-            Console.WriteLine(resultado);
+                        double resultado = (double)d / e;
+                        Console.WriteLine(resultado);
             */
 
 
