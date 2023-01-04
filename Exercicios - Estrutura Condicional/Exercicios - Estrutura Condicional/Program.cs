3ed683a151adc72c81dfa882019b2911e3cb081d
Luis Felipe <felipeadriani2@gmail.com>
2023-01-04 16:54:56 -0300

    Exercicios Estrutura Condicional

--- Exercicios - Estrutura Condicional/Exercicios - Estrutura Condicional/Program.cs
+++ Exercicios - Estrutura Condicional/Exercicios - Estrutura Condicional/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace exercicios {
+    class EstruturaCondicional {
+        static void Main(string[] args) {
+
+            //Exercicio 1
+            /*
+                        Console.Write("Digite um numero inteiro: ");
+                        int n1 = int.Parse(Console.ReadLine());
+
+                        if (n1 > 0) {
+                            Console.WriteLine("O numero digitado é positivo");
+                        }
+                        else if (n1 < 0) {
+                            Console.WriteLine("O numero digitado é negativo");
+                        }
+                        else {
+                            Console.WriteLine("O numero é zero");
+                        }
+
+
+                        //Exercicio 2
+
+                        Console.Write("Digite um numero inteiro: ");
+                        int n2 = int.Parse(Console.ReadLine());
+
+                        if (n2 % 2 == 0) {
+                            Console.WriteLine("O numero digitado é par");
+                        }
+                        else {
+                            Console.WriteLine("O numero é impar");
+                        }
+
+            */
+
+            //Exercicio 3
+
+            Console.Write("Digite um numero inteiro: ");
+            int n3 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro numero inteiro: ");
+            int n4 = int.Parse(Console.ReadLine());
+
+            if (n3 % n4 == 0) {
+                Console.WriteLine("São multiplos");
+            }
+            else if (n4 % n3 == 0) {
+                Console.WriteLine("São multiplos");
+            }
+            else {
+                Console.WriteLine("Não são multiplos");
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
