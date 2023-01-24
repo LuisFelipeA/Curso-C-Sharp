fc715d988dce6d5cdf32f0586d6d291610105994
Luis Felipe <felipeadriani2@gmail.com>
2023-01-24 09:03:42 -0300

    Encapsulamento

--- Secao 5/Secao 5/Produto.cs
+++ Secao 5/Secao 5/Produto.cs
@@ -8,31 +8,57 @@ using System.Threading.Tasks;
 namespace Construtores_this_sobrecarga_encapsulamento {
     internal class Produto {
 
-        public string Nome;
-        public double Preco;
-        public int Quantidade;
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
         
         public Produto(string nome, double preco, int quantidade) {
-            Nome = nome;
-            Preco = preco;
-            Quantidade = quantidade;
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+
+
+        public string GetNome() {
+            return _nome;
+        }
+
+        public void SetNome(string nome) {
+            if (nome != null && nome.Length > 1) {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() {
+            return _preco;
+        }
+
+        public int GetQuantidade() {
+            return _quantidade;
+        }
+
+        public Produto(string nome, double preco) {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0;
         }
 
 
         public double ValorTotalEstoque() {
-            return Preco * Quantidade;
+            return _preco * _quantidade;
         }
 
         public void AdicionarProdutos(int quantidade) {
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProdutos(int quantidade) {
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString() {
-            return "Produto: " + Nome + "\r\nPreço: $" + Preco.ToString("F2",CultureInfo.InvariantCulture) + "\r\nQuantidade: " + Quantidade;
+            return "Produto: " + _nome + "\r\nPreço: $" + _preco.ToString("F2",CultureInfo.InvariantCulture) + "\r\nQuantidade: " + _quantidade;
 
         }
     }
--- Secao 5/Secao 5/Program.cs
+++ Secao 5/Secao 5/Program.cs
@@ -4,17 +4,30 @@ using System.Globalization;
 namespace Construtores_this_sobrecarga_encapsulamento {
     class Program {
         static void Main(string[] args) {
-            
 
+            //encapsulamento
+
+            Produto p = new Produto("Tv", 500.00, 10);
+
+            Console.WriteLine(p.GetNome());
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+            /*
             Console.WriteLine("Entre os dados do produto:");
             Console.Write("Nome: ");
             string nome = Console.ReadLine();
             Console.Write("Preço: ");
             double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
             Console.Write("Quantidade no estoque: ");
             int quantidade = int.Parse(Console.ReadLine());
             
             Produto p = new Produto(nome, preco, quantidade);
+
+            Produto p = new Produto(nome, preco);
             
             Console.WriteLine();
             Console.WriteLine("Dados do produto: " + p);
@@ -30,7 +43,7 @@ namespace Construtores_this_sobrecarga_encapsulamento {
             p.RemoverProdutos(qte);
             Console.WriteLine();
             Console.WriteLine("Dados atualizados: " + p);
-
+            */
         }
     }
 }
