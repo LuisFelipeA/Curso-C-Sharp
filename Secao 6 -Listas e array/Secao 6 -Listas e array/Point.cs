0eaa9e68686cca6e08becdc61dcc8bb7252c2755
Luis Felipe <felipeadriani2@gmail.com>
2023-01-25 14:09:16 -0300

    Nullable

---
+++ Secao 6 -Listas e array/Secao 6 -Listas e array/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao_6__Listas_e_array {
+     struct Point {
+        public double x;
+        public double y;
+
+        public override string ToString() {
+            return "(" + x + ", " + y + ")";
+        }
+    }
+}
---
+++ Secao 6 -Listas e array/Secao 6 -Listas e array/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Secao_6__Listas_e_array {
+    class Program {
+        static void Main(string[] args) {
+            /*
+            Point p;
+            p.x = 10;
+            p.y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+
+
+            double? x = null;
+            double? y = 10.0;
+
+            //mostra o valor da variavel ou valor default caso seja null
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            //verifica se a variavel possui valor ou não
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue) {
+                Console.WriteLine(x.Value);
+            }
+            else {
+                Console.WriteLine("X is null");
+            }
+            if (y.HasValue) {
+                Console.WriteLine(y.Value);
+            }
+            else {
+                Console.WriteLine("Y is null");
+            }*/
+
+            double? x = null;
+            double? y = 10;
+
+            double a = x ?? 5;
+            double b = y ?? 5;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
