a344cfe4bc444a57b8e61578ef3dba7849506f30
Luis Felipe <felipeadriani2@gmail.com>
2023-01-25 10:04:35 -0300

    Exercicio Movimentação Conta Bancaria

---
+++ Secao 5/Exercicio Seção 5/Exercicio Seção 5/Conta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Seção_5 {
+    internal class Conta {
+
+        private int _numeroConta;
+        public string Nome { get; set;}
+        public double Saldo { get; private set; }
+        
+        public Conta (int numeroConta, string nome) {
+            _numeroConta = numeroConta;
+            Nome = nome;
+            Saldo = 0;
+        }
+
+        public void Deposito(double deposito) {
+            Saldo = Saldo + deposito;
+        }
+
+        public void Saque(double saque) {
+            Saldo = Saldo - saque - 5;
+        }
+
+        public override string ToString() {
+            return "Conta: " + _numeroConta + ", Titular: " + Nome + ", Saldo: $" + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Secao 5/Exercicio Seção 5/Exercicio Seção 5/Program.cs
@@ -0,0 +1,42 @@
+﻿using Exercicio_Seção_5;
+using System;
+using System.Globalization;
+
+namespace Exercicio {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o numero da conta: ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Havera deposito inicial: ");
+            char depositoInicial = char.Parse(Console.ReadLine());
+            
+            Conta conta1 = new Conta(conta, nome); 
+
+            if (depositoInicial == 's' || depositoInicial == 'S' ) {
+                Console.Write("Digite o deposito inicial: ");
+                double saldo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta1.Deposito(saldo);
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta1);
+
+            Console.Write("Digite o valor do deposito: ");
+            conta1.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta1);
+            
+            Console.Write("Digite o valor do saque: ");
+            conta1.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta1);
+
+
+        }
+    }
+}
