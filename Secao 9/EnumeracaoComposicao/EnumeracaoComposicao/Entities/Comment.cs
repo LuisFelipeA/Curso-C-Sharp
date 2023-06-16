66f1a26eb086022b967600fe952d52ffd77cad7b
Luis Felipe <felipeadriani2@gmail.com>
2023-06-16 10:20:31 -0300

    String Builder

---
+++ Secao 9/EnumeracaoComposicao/EnumeracaoComposicao/Entities/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace EnumeracaoComposicao.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+        public Comment() { }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
