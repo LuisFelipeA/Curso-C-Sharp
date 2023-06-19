a5ab3c7b07ce011e7a5d59019d827c76524f7046
Luis Felipe <felipeadriani2@gmail.com>
2023-06-19 10:03:53 -0300

    Herança

---
+++ Secao 10/HerancaPolimorfismo/HerancaPolimorfismo/Entities/Account.cs
@@ -0,0 +1,28 @@
+﻿namespace HerancaPolimorfismo.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ Secao 10/HerancaPolimorfismo/HerancaPolimorfismo/Program.cs
@@ -0,0 +1,30 @@
+﻿using HerancaPolimorfismo.Entities;
+
+namespace HerancaPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1, "Luis", 1273450.15, 100000.00);
+            
+            Console.WriteLine(account.Balance);
+
+            account.Deposit(300000);
+
+            Console.WriteLine(account.Balance);
+
+            account.WithDraw(23000);
+
+            Console.WriteLine(account.Balance);
+
+            account.Loan(110000);  //passando do limite
+
+            Console.WriteLine(account.Balance);
+
+            account.Loan(95000);
+
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
