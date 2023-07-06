b977e66f109dc170ef4b8b2c15f5c490bb0eecbd
Luis Felipe <felipeadriani2@gmail.com>
2023-07-06 11:04:48 -0300

    Introduction LINQ

--- Secao17/ExpressaoLambdaDelegatesLINQ/ExpressaoLambdaDelegatesLINQ/Program.cs
+++ Secao17/ExpressaoLambdaDelegatesLINQ/ExpressaoLambdaDelegatesLINQ/Program.cs
@@ -41,7 +41,6 @@ namespace ExpressaoLambdaDelegatesLINQ
                 Console.WriteLine(s);
             }
 
-
             /**/
 
 
---
+++ Secao17/LINQ/LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5, };
+
+            // Define the query expression
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            // Execute the query
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
