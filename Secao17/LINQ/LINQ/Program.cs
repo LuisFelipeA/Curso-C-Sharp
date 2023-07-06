2e94452b0a8460c7e5d0835021f3031d642726da
Luis Felipe <felipeadriani2@gmail.com>
2023-07-06 15:59:43 -0300

    LINQ with Lambda  part 2

--- Secao17/LINQ/LINQ/Program.cs
+++ Secao17/LINQ/LINQ/Program.cs
@@ -66,6 +66,44 @@ namespace LINQ
             var r9 = products.Where(p => p.Id == 30).SingleOrDefault();
             Console.WriteLine("Single or default test2: " + r9);
 
+            var r10 = products.Max(p => p.Price);
+            Console.WriteLine("Max price: " + 10); 
+
+            var r11 = products.Min(p => p.Price);
+            Console.WriteLine("Min price: " + 11);
+
+            var r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+            Console.WriteLine("Category 1 Sum prices: " + r12);
+
+            var r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+            Console.WriteLine("Category 1 Avarage prices: " + r13);
+
+            var r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Category 5 Average prices: " + r14);
+
+            var r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate(0.0, (x,y) => x + y);
+            Console.WriteLine("Category 1 aggregate sum: ", r15);
+
+            Console.WriteLine();
+
+            var r16 = products.GroupBy(p => p.Category);
+            foreach (IGrouping<Category, Product> group in r16)
+            {
+                Console.WriteLine("Category " + group.Key.Name + ":");
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
             /* Introdução LINQ
             
             // Specify the data source
